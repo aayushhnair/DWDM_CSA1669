6dd2d75e9a384ca2180d0da66ab7c8c1955598a9
Ayush V Nair <96865576+xakvn@users.noreply.github.com>
2023-01-10 11:26:13 +0530

    Delete LinearRegression.r

--- LinearRegression.r
+++
@@ -1,8 +0,0 @@
-data <- read.csv("diabetes.csv")
- rel <- lm(data$BloodPressure ~ data$Age)
- png <- (file = "LinearRegression.png")
-plot(data$Age, data$BloodPressure, col="blue", main=  "Linear Regression Analysis" , 
-      abline= (lm(data$BloodPressure~ data$Age)), xlab = "BloodPressure", ylanb= "Age")
-A<- data.frame(data$Age)
-Result<- predict(rel, A)
-print(Result)
